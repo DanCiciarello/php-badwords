01a18fe7dfc91c3be72f8a6e7730342273d662ee
DanCiciarello <me@danieleciciarello.com>
2022-07-07 16:08:30 +0200

    Update

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
     $paragraph = "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, 
     there live the blind text. Separated they live in Bookmarksgrove right at the coast of the Semantics, 
     a large language ocean. A small river named Duden flows by their place and supplies it with the necessary 
@@ -9,7 +10,12 @@
     wild Question Marks and devious Semikoli, but the Little blind text didn’t listen. She packed her seven 
     versalia, put her initial into the belt and made herself on the way. When she reached the first hills 
     of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the blind text 
-    of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then."
+    of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then.";
+
+    $censured = $GET["censured"];
+
+    $modified = str_replace($censured, "***", $paragraph);
+
 ?>
 
 <!DOCTYPE html>
@@ -21,6 +27,17 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    <p><?php echo $paragraph ?></p>
+    <div>
+        <h1>Paragrafo originale</h1>
+        <p><?php echo $paragraph ?></p>
+        <p>La lunghezza del testo sopraelencato è di <strong><?php echo strlen($paragraph) ?></strong> caratteri.</p>
+    </div>
+    <hr>
+    <div>
+        <h1>Paragrafo censurato</h1>
+        <h4>Censura della parola "<?php echo $censured ?>"</h4>
+        <p><?php echo $modified ?></p>
+        <p>La lunghezza del testo sopraelencato è di <strong><?php echo strlen($modified) ?></strong> caratteri.</p>
+    </div>
 </body>
 </html>
